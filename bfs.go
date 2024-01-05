920718f542a7be1994ed2b96c4dbd4488695b0b4
Leonardo Da Pozzo <l.dapozzo398@gmail.com>
2024-01-05 20:12:29 +0100

    refactoring; use channels instead of mutexes

--- bfs.go
+++ bfs.go
@@ -1,56 +1,101 @@
 package main
 
 import (
+	"errors"
+	"math"
 	"sync"
 
 	"github.com/awalterschulze/gographviz"
 )
 
-// Perform a BFS on the provided graph, sending every new node on the 'out_ch' channel
-func bfs(graph gographviz.Graph, out_ch chan string, done_ch chan bool) {
-	// TODO: check null pointer?
+// Holds the name of a node and its distance from the starting node
+type NodeData struct {
+	Name string
+	Dist int
+}
+
+// Perform a BFS on the provided graph, sending data about every new node on the
+// 'out_ch' channel and communicating the end of the search through the
+// 'done_ch' channel
+func BFS(graph gographviz.Graph, out_ch chan NodeData, done_ch chan bool) error {
+	if graph.Nodes == nil || len(graph.Nodes.Nodes) == 0 || graph.Nodes.Nodes[0] == nil {
+		return errors.New("Missing a starting node for the search.")
+	}
+
 	starting_node := *graph.Nodes.Nodes[0]
+
+	// Keeps track of already-explored nodes
 	explored := make(map[string]bool)
+	// Maps each node to its distance from the starting node
+	distances := make(map[string]int)
+	// The current node frontier
 	var frontier []gographviz.Node
 	frontier = append(frontier, starting_node)
+
+	distances[starting_node.Name] = 0
 
 	for len(frontier) != 0 {
 		// Pop the head of the frontier
 		node := frontier[0]
 		frontier = frontier[1:]
 
-		// Skip node if already explored
 		if explored[node.Name] {
 			continue
 		}
 
-		// Mark this node as explored to avoid revisiting it
+		// Only update the distance if it is smaller than the currently recorded one
+		neighbours := getNeighbours(graph, node)
+		var neighbours_to_update []gographviz.Node
+		for _, neighbour := range neighbours {
+			old_dist, ok := distances[neighbour.Name]
+			new_dist := distances[node.Name] + 1
+
+			// We only update the distance if it's shorter than the one currently recorder
+			if !ok || new_dist < old_dist {
+				distances[neighbour.Name] = new_dist
+				neighbours_to_update = append(neighbours_to_update, neighbour)
+			}
+		}
+		frontier = append(frontier, neighbours_to_update...)
+
 		explored[node.Name] = true
-
-		// Update the frontier with this node's neighbours
-		neighbours := get_neighbours(graph, node)
-		frontier = append(frontier, neighbours...)
-
-		out_ch <- node.Name
+		out_ch <- NodeData{node.Name, distances[node.Name]}
 	}
 
 	done_ch <- true
+	return nil
 }
 
-// TODO: desc
-func parallel_bfs(graph gographviz.Graph, out_ch chan string, done_ch chan bool) {
-	// TODO: check null pointer?
+// Perform a BFS on the provided graph, sending data about every new node on the
+// 'out_ch' channel and communicating the end of the search through the
+// 'done_ch' channel
+func ParallelBFS(graph gographviz.Graph, out_ch chan NodeData, done_ch chan bool) error {
+	if graph.Nodes == nil || len(graph.Nodes.Nodes) == 0 || graph.Nodes.Nodes[0] == nil {
+		return errors.New("Missing a starting node for the search.")
+	}
+
 	starting_node := *graph.Nodes.Nodes[0]
+
+	// Keeps track of already-explored nodes
 	explored := make(map[string]bool)
-	var frontier []gographviz.Node
-	frontier = append(frontier, starting_node)
 
+	// Channel to request the current frontier
 	req_frontier_ch := make(chan bool)
+	// Channel to receive the current frontier
 	frontier_ch := make(chan []gographviz.Node)
+	// Channel to append nodes to the frontier
 	append_ch := make(chan []gographviz.Node)
-	cleanup_frontier_ch := make(chan bool)
 
-	go maintain_frontier(frontier, req_frontier_ch, frontier_ch, append_ch, cleanup_frontier_ch)
+	// Channel to request the distance of a node
+	req_distance_ch := make(chan string)
+	// Channel to receive the distance of a node
+	distance_ch := make(chan int)
+	// Channel to update the distance of a node
+	update_distance_ch := make(chan NodeData)
+
+	// Access to the frontier and the distance data are handled by dedicated goroutines
+	go maintainFrontier(starting_node, req_frontier_ch, frontier_ch, append_ch, done_ch)
+	go maintainDistances(starting_node, req_distance_ch, distance_ch, update_distance_ch, done_ch)
 
 	var wg sync.WaitGroup
 
@@ -59,6 +104,7 @@ func parallel_bfs(graph gographviz.Graph, out_ch chan string, done_ch chan bool)
 		req_frontier_ch <- true
 		current_frontier := <-frontier_ch
 
+		// No more nodes in the frontier: we are done
 		if len(current_frontier) == 0 {
 			break
 		}
@@ -73,19 +119,40 @@ func parallel_bfs(graph gographviz.Graph, out_ch chan string, done_ch chan bool)
 			wg.Add(1)
 			go func(to_process gographviz.Node) {
 				defer wg.Done()
-				append_ch <- get_neighbours(graph, to_process)
-				out_ch <- to_process.Name
+				neighbours := getNeighbours(graph, to_process)
+				var neighbours_to_update []gographviz.Node
+
+				req_distance_ch <- to_process.Name
+				current_node_dist := <-distance_ch
+
+				for _, neighbour := range neighbours {
+					req_distance_ch <- neighbour.Name
+					old_dist := <-distance_ch
+					new_dist := current_node_dist + 1
+
+					// We only update the distance if it's shorter than the one currently recorder
+					if new_dist < old_dist {
+						update_distance_ch <- NodeData{neighbour.Name, new_dist}
+						neighbours_to_update = append(neighbours_to_update, neighbour)
+					}
+				}
+				append_ch <- neighbours_to_update
+
+				out_ch <- NodeData{to_process.Name, current_node_dist}
 			}(node)
 		}
 
+		// We need to wait for all goroutines to append their newly found nodes
+		// to the frontier in order to avoid a premature exit
 		wg.Wait()
 	}
 
-	done_ch <- true
+	close(done_ch)
+	return nil
 }
 
 // Return an array containing all the neighbours of the provided node
-func get_neighbours(graph gographviz.Graph, node gographviz.Node) []gographviz.Node {
+func getNeighbours(graph gographviz.Graph, node gographviz.Node) []gographviz.Node {
 	var neighbours []gographviz.Node
 	// All nodes immediately reachable from this node
 	for dst := range graph.Edges.SrcToDsts[node.Name] {
@@ -100,14 +167,44 @@ func get_neighbours(graph gographviz.Graph, node gographviz.Node) []gographviz.N
 }
 
 // Manage access to the frontier, appending received nodes and sending the current state of the frontier when requested
-func maintain_frontier(frontier []gographviz.Node, req_frontier_ch chan bool, frontier_ch chan []gographviz.Node, append_ch chan []gographviz.Node, done_ch chan bool) {
+func maintainFrontier(starting_node gographviz.Node, req_frontier_ch chan bool, frontier_ch chan []gographviz.Node, append_ch chan []gographviz.Node, done_ch chan bool) {
+	// The current node frontier
+	var frontier []gographviz.Node
+	frontier = append(frontier, starting_node)
+
 	for {
 		select {
 		case <-req_frontier_ch:
+			// Send the frontier, as requested
 			frontier_ch <- frontier
 			frontier = nil
 		case nodes := <-append_ch:
+			// Append the received nodes to the frontier
 			frontier = append(frontier, nodes...)
+		case <-done_ch:
+			return
+		}
+	}
+}
+
+func maintainDistances(starting_node gographviz.Node, req_distance_ch chan string, distance_ch chan int, update_distance_ch chan NodeData, done_ch chan bool) {
+	// Maps each node to its distance from the starting node
+	distances := make(map[string]int)
+	distances[starting_node.Name] = 0
+
+	for {
+		select {
+		case node_name := <-req_distance_ch:
+			// Send the requested node's distance
+			dist, ok := distances[node_name]
+			if !ok {
+				// This is the first time seeing the node, so we set its distance to the maximum
+				dist = math.MaxInt32
+			}
+			distance_ch <- dist
+		case node_data := <-update_distance_ch:
+			// Update the distance with the provided data
+			distances[node_data.Name] = node_data.Dist
 		case <-done_ch:
 			return
 		}
--- main.go
+++ main.go
@@ -4,18 +4,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"sync"
 	"time"
 
 	"github.com/awalterschulze/gographviz"
 )
 
-// Panic in case of error
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+type explorationStrategy func(gographviz.Graph, chan NodeData, chan bool) error
 
 func main() {
 	if len(os.Args[1:]) < 1 {
@@ -23,71 +19,99 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Read the contents of the file specified by the user
-	var path = os.Args[1]
-	contents, err := os.ReadFile(path)
-	check(err)
+	graph, err := deserializeGraph(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// TODO: check null pointer?
-	graph := *parseGraph(contents)
-	check(err)
+	if graph == nil {
+		log.Fatal("No graph found.")
+	}
 
 	fmt.Println("--- Sequential ---")
-	explore_graph(graph, true)
+	explore_graph(*graph, BFS)
+
+	fmt.Println()
+
 	fmt.Println("--- Parallel ---")
-	explore_graph(graph, false)
+	explore_graph(*graph, ParallelBFS)
 }
 
 // Parse the provided bytes into a DOT graph and return it
-func parseGraph(data []byte) *gographviz.Graph {
-	graphAst, err := gographviz.Parse(data)
-	check(err)
+func deserializeGraph(path string) (*gographviz.Graph, error) {
+	// Read file contents
+	contents, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse the file contents into an AST
+	graphAst, err := gographviz.Parse(contents)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a graph from the AST
 	graph := gographviz.NewGraph()
 	err = gographviz.Analyse(graphAst, graph)
-	check(err)
+	if err != nil {
+		return nil, err
+	}
 
-	return graph
+	return graph, nil
 }
 
-// Explore the graph with either a sequential or parallel search
-func explore_graph(graph gographviz.Graph, seq bool) {
-	result_ch := make(chan string)
+// Explore the graph with the provided strategy
+func explore_graph(graph gographviz.Graph, strategy explorationStrategy) {
+	// Channel for communicating data about the explored nodes
+	node_data_ch := make(chan NodeData)
+	// Channel for communicating the end of the exploration
 	done_ch := make(chan bool)
 
 	var wg sync.WaitGroup
 	wg.Add(2)
 	start := time.Now()
+	// Start the exploration in a new goroutine
 	go func() {
 		defer wg.Done()
-		if seq {
-			bfs(graph, result_ch, done_ch)
-		} else {
-			parallel_bfs(graph, result_ch, done_ch)
-		}
+		strategy(graph, node_data_ch, done_ch)
 	}()
+	// Start the monitoring function in a new goroutine
 	go func() {
 		defer wg.Done()
-		gather_results(result_ch, done_ch)
+		gather_results(node_data_ch, done_ch)
 	}()
 	wg.Wait()
 	end := time.Now()
 	fmt.Println("Finished successfully in ", end.Sub(start).Microseconds(), "us.")
 }
 
-// Gather results from result_ch and print them until a message arrives on done_ch
-func gather_results(result_ch chan string, done_ch chan bool) {
-	var explored_nodes_list []string
+// Gather information about the nodes and print it once the exploration is over
+func gather_results(node_data_ch chan NodeData, done_ch chan bool) {
+	node_distances := make(map[string]int)
 
 	for {
 		select {
-		case res := <-result_ch:
-			explored_nodes_list = append(explored_nodes_list, res)
+		case node := <-node_data_ch:
+			// Update the distance for the newly received node
+			node_distances[node.Name] = node.Dist
 		case <-done_ch:
-			// fmt.Println("Nodes:")
-			// for _, node := range explored_nodes_list {
-			// 	fmt.Println(node)
-			// }
-			fmt.Println("Explored", len(explored_nodes_list), "nodes.")
+			// Print information about all the received nodes and return
+			fmt.Println("Explored", len(node_distances), "nodes.")
+			fmt.Println("Nodes:")
+
+			// Sort nodes by their distance from the starting node
+			var sorted []NodeData
+			for name, dist := range node_distances {
+				sorted = append(sorted, NodeData{name, dist})
+			}
+			sort.Slice(sorted, func(i, j int) bool {
+				return sorted[i].Dist < sorted[j].Dist
+			})
+
+			for _, node := range sorted {
+				fmt.Println(node.Name, "at distance", node.Dist)
+			}
 			return
 		}
 	}
